d8c8b0e0625c4fe4f9b7e0fff81e52bbc318b2a0
haitrieu123580 <haitrieu123580@gmail.com>
2023-03-22 09:58:04 +0700

    learn RESTapi

--- Learn/RESTapi/client.js
+++ Learn/RESTapi/client.js
@@ -1,5 +1,15 @@
 fetch('http://localhost:8080/listUsers')
 .then(response => response.json())
   .then(data => {
+    let  html =  `<ul>
+    <li>
+      <div>${data.user1.name}  
+      </div>
+      <div>${data.user2.password}
+      </div>
+    </li>
+    </ul>`
+    box.insertAdjacentHTML("afterend", html);
     console.log(data)
   });
+  const box  = document.querySelector('.box')
