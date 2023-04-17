ab38e2e0c631171fe27b3114b714c4957363de40
haitrieu123580 <haitrieu123580@gmail.com>
2023-04-17 17:36:38 +0700

    authenication with passport

---
+++ library_management/model/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql')
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
+  connection.connect((err) =>{
+    if(err) throw err
+    console.log('connected database!')
+})
+module.exports = connection
